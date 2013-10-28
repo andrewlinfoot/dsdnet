fe6aa0c5c8b16d2e848f7aca20ea5408b9bf0271
Andrew Linfoot <andrewlinfoot@Andrews-MacBook-Air.local>
2013-10-27 21:35:47 -0400

    updated collections.js schema comments

--- collections.js
+++ collections.js
@@ -1,24 +1,31 @@
 // {
-// 	_id: product id,
-// 	name: product name,
-// 	other info goes here...
+// 	"_id" : "ewebnBbuSniYgc8FR",
+// 	"productNumber" : 82799,
+// 	"stock" : 0,
+// 	"brand" : "APPLE&EVE",
+// 	"pack" : "40",
+// 	"description" : "Juice Tropical Twist 40/4.23 *Cs",
+// 	"category" : "9CBbbR6GsYPmvxNaT",
+// 	"slug" : "Juice_Tropical_Twist_40/4.23_*Cs"
 // }
 Products = new Meteor.Collection('products');
 
 // {
-// 	_id: categories id,
-// 	name: category name,
-// 	parent: parent categories id || null
+// 	"_id" : "7MAu2YYDHSuujsnrd",
+// 	"company" : "iZPkLZ25Tn9NFvdEi",
+// 	"name" : "Dairy",
+// 	"parent" : null,
+// 	"slug" : "Dairy"
 // }
 Categories = new Meteor.Collection('categories');
 
 // {
-// 	_id: companies id,
-//	url: company page url
-// 	name: company name,
-// 	description: company description,
-// 	address: company address,
-// 	phone: company phone number,
-// 	website: company website
+// 	"_id" : "iZPkLZ25Tn9NFvdEi",
+// 	"address" : "333 West 14th Street Indianapolis, IN 46202",
+// 	"description" : "From our humble beginnings in 1948, McFarling Foods grew to become one of the largest independently-owned foodservice distributors in Indiana and a shareholder in Unipro Foodservice Inc., the world's largest foodservice cooperative. In 2009, we're celebrating our next step - becoming Indiana's largest 100% employee-owned food distributor. We are proud to say that when you call McFarling Foods, you're always speaking to an owner. Although our ownership structure has changed, our customers know they can expect the same commitment to service and quality brands that have helped the company become what it is today. McFarling Foods sells CODE, COMPANIONS, CORTONA, and WORLD HORIZON label products, in addition to hundreds of familiar national-labeled lines. McFarling Foods manufactures many products in our modern USDA-inspected meat and poultry departments. We have fresh seafood arriving daily and stock broadline inventories of fresh produce, fluid milk, and ice cream. Extensive frozen, canned, dry, disposable and chemical lines complete our product line. Our customers include renowned independent fine dining, deli, catering and concession, hospital and healthcare, industrial and institutional foodservice operators. McFarling Foods has provided national brands and programs through the local connection to our community for over a half-century. Arrange to visit our facility in Indianapolis. We'll show you our investment in the future.",
+// 	"name" : "McFarling Foods",
+// 	"phone" : "(800) 622-9003",
+// 	"slug" : "McFarling_Foods",
+// 	"website" : "http://www.mcfarling.com/"
 // }
 Companies = new Meteor.Collection('companies');

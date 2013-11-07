b11625cc2ac8764025e50d0df66218e775199b3d
Patryk <patryk.ozga@outlook.com>
2013-11-07 15:40:04 -0500

    add demo package

---
+++ packages/demo-bakery/package.js
@@ -0,0 +1,8 @@
+Package.describe({
+    summary: 'set dummy data to bread category in mongo'
+});
+
+Package.on_use(function (api) {
+  api.add_files('breadwriter.js', 'server');
+  api.export('BreadWriter');
+});
